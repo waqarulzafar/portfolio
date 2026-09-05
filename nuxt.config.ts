989d837af1e58071bbaea1658415e455237206a1
waqarulzafar <waqarulzafar@gmail.com>
2026-09-06 00:00:49 +0500

    ok

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   ],
 
   devtools: { enabled: false },
-  colorMode: { preference: 'light' },
+  colorMode: { preference: 'dark' },
   icon: {
     provider: 'none',
     clientBundle: {
