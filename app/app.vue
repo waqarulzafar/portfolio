8ead13e81e073d3aeb9182b951fbdfd8f3a3b643
waqarulzafar <waqarulzafar@gmail.com>
2026-09-05 10:19:55 +0500

    Add HubSpot chatbot and updated cutout portrait

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-useHead({ htmlAttrs: { lang: 'en' }, link: [{ rel: 'icon', href: '/favicon.svg' }] })
+useHead({ script: [{ id: 'hs-script-loader', src: 'https://js-na2.hs-scripts.com/247292806.js', type: 'text/javascript', async: true, defer: true, tagPosition: 'bodyClose' }], htmlAttrs: { lang: 'en' }, link: [{ rel: 'icon', href: '/favicon.svg' }] })
 useSeoMeta({ title: 'Waqar UL Zafar | Full-stack & Mobile App Developer', description: '7 years of experience building websites, Android and iOS apps, and AI solutions. Web design from $100, app development from $150, and ready-made POS systems for $300.' })
 </script>
 <template><UApp><NuxtPage /></UApp></template>
